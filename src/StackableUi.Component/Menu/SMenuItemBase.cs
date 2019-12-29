0868039e86b3535e918c6f42c809fb3b934cf1a2
辉 冯 <695979933@qq.com>
2019-12-29 15:55:22 +0800

    添加Route

---
+++ src/StackableUi.Component/Menu/SMenuItemBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace StackableUi.Component.Menu
+{
+    public class SMenuItemBase:SComponentBase
+    {
+        [Parameter]
+        public string Route { get; set; }
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+        public void OnClick()
+        {
+            if (!string.IsNullOrEmpty(Route))
+            {
+                NavigationManager.NavigateTo(Route);
+            }
+        }
+    }
+}
