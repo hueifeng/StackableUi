7be913b0a79fcc5bc7df7c0b9853f2d5a5e35d2d
辉 冯 <695979933@qq.com>
2020-01-01 22:09:35 +0800

    增加Input类型 输入框类型，可选值为 text、password、textarea、url、email、date、number、tel

---
+++ src/StackableUi.Component/Input/InputType.cs
@@ -0,0 +1,18 @@
+﻿namespace StackableUi.Component.Input
+{
+    /// <summary>
+    ///     Input类型
+    /// </summary>
+    public enum InputType
+    {
+        Text,
+        PassWord,
+        TextArea,
+        Url,
+        Email,
+        Date,
+        Number,
+        Tel
+
+    }
+}
