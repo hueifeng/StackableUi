90aaf22ab07eeea571024595b81bbb7b0e6a66ec
HueiFeng <695979933@qq.com>
2020-01-18 11:28:26 +0800

    增加Switch控件

---
+++ src/StackableUi.Component/Switch/SwitchSize.cs
@@ -0,0 +1,17 @@
+﻿namespace StackableUi.Component.Switch
+{
+    /// <summary>
+    ///     尺寸
+    /// </summary>
+    public enum SwitchSize:byte
+    {
+        /// <summary>
+        ///     大
+        /// </summary>
+        Large,
+        /// <summary>
+        ///     小
+        /// </summary>
+        Small
+    }
+}
